c4c0ba41df0a2765c3a11e20f4d2d3f3012433bc
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 16:46:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
-require 'pry'
+
 def square_array(array)
   output_array = []
   array.each do |forEach|
     squared = forEach**2
     output_array << squared
   end
-  binding.pry
   output_array
 end
