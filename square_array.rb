48f4ea988784ad54015ae217b79b268b7f39d75d
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 16:47:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 
 def square_array(array)
   output_array = []
-  array.each do |forEach|
-    squared = forEach**2
+  array.each do |value|
+    squared = value**2
     output_array << squared
   end
   output_array
